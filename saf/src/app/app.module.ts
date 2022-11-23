819b325369468bc09c1d0aa2c6ae57f1bd7d79b2
Racz Csaba <legoscsabi@gmail.com>
2022-11-23 22:17:50 +0100

    test

--- saf/src/app/app.module.ts
+++ saf/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { StudentsComponent } from './students/students.component';
+import { StudygroupsComponent } from './studygroups/studygroups.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StudentsComponent,
+    StudygroupsComponent
   ],
   imports: [
     BrowserModule,
---
+++ saf/src/app/dataInterface.ts
@@ -0,0 +1,13 @@
+export interface students {
+
+  name:string,
+  sex:boolean,
+  birthplace:string,
+  birthdate:Date,
+  email:string,
+  studyGroups:string[]
+}
+
+export interface studyGroups {
+  name:string
+}
